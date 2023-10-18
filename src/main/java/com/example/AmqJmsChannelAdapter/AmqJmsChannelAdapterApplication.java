10c9efe3c08284a7297f251548ccfccae5ac50f2
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-18 19:39:44 -0400

    project init

---
+++ src/main/java/com/example/AmqJmsChannelAdapter/AmqJmsChannelAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmqJmsChannelAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqJmsChannelAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqJmsChannelAdapterApplication.class, args);
+	}
+
+}
